39f5776fdccadfc0471b2d632f8d2e9ad03584ef
Almxguer <aaronalmaguer304@gmail.com>
2025-07-07 13:25:44 -0600

    Quitar archivos de prueba del repositorio

--- test_chroma.py
+++
@@ -1,8 +0,0 @@
-import chromadb
-
-client = chromadb.PersistentClient(path="./chroma_db")
-collection = client.get_collection(name="tiktok_scripts")
-
-print("Documentos almacenados:")
-print(collection.peek(5))  
-
